c63c8949f9420eeb0ffb6399020e0271d8524d1a
Administrator <administrator@JICSTestingMac.hsd1.ma.comcast.net>
2018-07-10 21:51:58 -0400

    dding nodejs project

---
+++ nodejs/CurrencyApp/currency.js
@@ -0,0 +1,15 @@
+
+var Currency = function(amount){
+    this.amount = amount;
+}
+Currency.prototype.roundTwoDecimals = function(amount){
+    return Math.round(amount *100)/100;
+}
+Currency.prototype.canadianToUS = function ( canadian){
+    return roundTwoDecimals(canadian*canadianDollar);
+}
+
+Currency.prototype.USToCanadian  = function(us){
+    return roundTwoDecimals(us/canadianDollar);
+}
+exports = Currency;
