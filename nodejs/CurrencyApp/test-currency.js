47bb4866fa2303fc6fa7a3ae8f80a03c61c058a6
Administrator <administrator@JICS-Testing-Mac.local>
2018-07-26 10:09:15 -0400

    saving current progresse

--- nodejs/CurrencyApp/test-currency.js
+++ nodejs/CurrencyApp/test-currency.js
@@ -1,4 +1,4 @@
-var Currency = require('./currency');
+var Currency = require('currency');
 var currencyObject = new Currency(0.93)
 console.log('50 Canadian dollas equal this amount of us dollars');
 console.log(currencyObject.canadianToUS(50));
---
+++ nodejs/postsApp/blog_recent.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req,res){
+    if(req.url == '/'){
+        fs.readFile('./titles.json',function(err,data){
+            if(err){
+                console.error(err);
+                res.end('Server Error');
+            }else{
+                console.log("data");
+                console.log(data);
+                console.log(JSON.parse(data.toString()));
+                var titles = JSON.parse(data.toString());
+                fs.readFile('./template.html',function(err,data){
+                    if(err){
+                        console.error(err);
+                        res.end('Server Error');
+                    }else{
+                        var tmpl = data.toString();
+                        var html = tmpl.replace('%',titles.join('<li></li>'));
+                        res.writeHead(200,{'Content-type':'text/html'});
+                        res.end(html);
+                    }
+
+                })
+            }
+        })
+    }
+}).listen(8001,"127.0.0.1",()=>{
+    console.log('listening');
+})
