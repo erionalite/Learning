83e15341f3502b82afe0f0b6776db3be06d542e0
erionalite <erion.alite@gmail.com>
2024-04-16 18:02:07 -0400

    adding react app

---
+++ Ticktock/ticktock-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/Board'
+import Board from './components/Board';
+
+
+function App() {
+  return (
+     
+    <div className="App">
+      <h2>Board Game</h2>
+      <Board></Board>
+    </div>
+  );
+}
+
+export default App;
